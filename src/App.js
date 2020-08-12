2900be472d256fe03af5e28f7dce1a05d5e3df1e
bangjaehun <qkdwogns98@gmail.com>
2020-08-12 22:31:01 +0900

    NewsList

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=e14ec3724f794705b6d055eab86616c3',
-      );
-
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly
-        ></textarea>
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
