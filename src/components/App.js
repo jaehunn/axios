e31c2ffedb1a273b744f7e08547c7a791a949a4a
bangjaehun <qkdwogns98@gmail.com>
2020-08-13 19:36:00 +0900

    get data - api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
-import React from './react';
+import React from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div>testing</div>;
+  const [data, setData] = React.useState(null);
+  const handleClick = async () => {
+    try {
+      const { data } = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=021be107ec8e4910b929d7a25db26080',
+      );
+
+      setData(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={handleClick}>Load</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/index.js
@@ -0,0 +1,2 @@
+// api key: 021be107ec8e4910b929d7a25db26080
+export const categories = {};
