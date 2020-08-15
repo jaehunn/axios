d895fba37388cad88d3348eeb8fc4e9f3335414f
bangjaehun <qkdwogns98@gmail.com>
2020-08-15 13:51:17 +0900

    Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom';
-import NewsPage from './pages/NewsPage';
+import Routes from './Routes';
 
 function App() {
   return (
     <div className="App">
-      <Route path="/:category?" component={NewsPage} /> {/* category or not */}
+      <Routes />
     </div>
   );
 }
