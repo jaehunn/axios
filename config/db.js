4bfa9d94208ac5253f6a4368eb976c815d73b460
bangjaehun <qkdwogns98@gmail.com>
2020-10-27 22:13:21 +0900

    profile api...

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       userCreateIndex: true,
+      useFindAndModify: false,
     });
 
     console.log("MongoDB Connected...");
