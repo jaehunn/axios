629955cf4d92badf4739bd51f66827757014392f
bangjaehun <qkdwogns98@gmail.com>
2020-11-07 19:32:26 +0900

    Profile UI, addendum: in profile reducer, PROFILE_ERROR case add profile: null

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEduaction from "./components/profile-forms/AddEduaction";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import setAuthToken from "./utils/setAuthToken";
@@ -42,6 +43,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               {/* Protect Component: Dashboard */}
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -36,6 +36,7 @@ export default function (state = initialState, action) {
     case PROFILE_ERROR:
       return {
         ...state,
+        profile: null,
         error: payload,
         loading: false,
       };
