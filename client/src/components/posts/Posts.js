0a53e738063796e31b86407c4b56f49e482fdad7
bangjaehun <qkdwogns98@gmail.com>
2020-11-08 19:48:58 +0900

    post... (issue: new post submit -> no-render new post)

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -4,12 +4,29 @@ import { getPosts } from "../../actions/post";
 import PropTypes from "prop-types";
 
 import Spinner from "../layouts/Spinner";
+import PostItem from "./PostItem";
+import PostForm from "./PostForm";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
-  }, [getPosts]);
-  return <div></div>;
+  }, [getPosts, posts]); // ISSUE: update
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="larget text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"> Welcome to the community</i>
+      </p>
+      <PostForm />
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,10 @@
-import { GET_POSTS, POST_ERROR } from "../actions/types";
+import {
+  ADD_POST,
+  DELETE_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+} from "../actions/types";
 
 const initialState = {
   posts: [],
@@ -7,6 +13,7 @@ const initialState = {
   error: {},
 };
 
+// eslint-disable-next-line
 export default function (state = initialState, action) {
   const { type, payload } = action;
 
@@ -17,10 +24,30 @@ export default function (state = initialState, action) {
         posts: payload,
         loading: false,
       };
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [...state.posts, payload],
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter((post) => post._id !== payload),
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
         loading: false,
       };
     default:
